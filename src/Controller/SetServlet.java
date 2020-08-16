88d5871baad9bf014581b9ab7fa9de5afaaaac59
chaitanya <saichaitanya15236@gmail.com>
2020-08-16 16:16:10 +0530

    FSJ0620D107

--- src/Controller/SetServlet.java
+++ src/Controller/SetServlet.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,11 +16,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.TouristPlace;
 import service.ListOperations;
+import service.SetOperations;
 
 @WebServlet(urlPatterns= {"/set"})
 
 public class SetServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	Set<TouristPlace> set=new HashSet<TouristPlace>();
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String name = request.getParameter("name");
@@ -30,10 +35,13 @@ public class SetServlet extends HttpServlet {
 		String remove = request.getParameter("delete");
 		String reset = request.getParameter("reset");
 		
+		TouristPlace tp = new TouristPlace(name,destination,rank);
+		SetOperations sp=new SetOperations();
+		
 		if(add!=null) {
 			// call the add method and store the return value in a set variable
-			
-			request.setAttribute("bucketListadd", /*pass the return value */);
+			 set= sp.add(tp);
+			request.setAttribute("bucketListadd", set/*pass the return value */);
 			request.setAttribute("message", "user added successfully");
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/set.jsp");
 			rd.forward(request, response);
@@ -42,8 +50,8 @@ public class SetServlet extends HttpServlet {
 		if(remove!=null) {
 			// call the remove method and store the return value in a set variable
 			
-			
-			request.setAttribute("bucketListremove", /*pass the return value */);
+			set=sp.remove(tp);
+			request.setAttribute("bucketListremove", set/*pass the return value */);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/set.jsp");
 			rd.forward(request, response);
 		}
@@ -52,24 +60,25 @@ public class SetServlet extends HttpServlet {
 		if(sortbydestination!=null) {
 			// call the sortByDestination method and store the return value in a set variable
 			
+			TreeSet<TouristPlace>  se=(TreeSet<TouristPlace>) sp.sortByDestination(set);
 			
-			request.setAttribute("bucketList", /*pass the return value */);
+			request.setAttribute("bucketList", se/*pass the return value */);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/set.jsp");
 			rd.forward(request, response);
 		}
 
 		if(sortbyrank!=null) {
 			// call the sortByRank method and store the return value in a set variable
-			
-			request.setAttribute("bucketList", /*pass the return value */);
+			TreeSet<TouristPlace> sb= (TreeSet<TouristPlace>) sp.sortByRank(set);
+			request.setAttribute("bucketList",sb /*pass the return value */);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/set.jsp");
 			rd.forward(request, response);
 		}
 
 		if(reset!=null) {
 			// call the reset method and store the return value in a set variable
-			
-			request.setAttribute("bucketList", /*pass the return value */);
+			Set<TouristPlace> rs =(Set<TouristPlace>) sp.reset(set);
+			request.setAttribute("bucketList",rs /*pass the return value */);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/set.jsp");
 			rd.forward(request, response);
 		}
--- src/service/ListOperations.java
+++ src/service/ListOperations.java
@@ -1,1 +1,36 @@
 package service;
+
+import java.util.*;
+
+import model.TouristPlace;
+
+public class ListOperations
+{
+	List<TouristPlace> list = new ArrayList<TouristPlace>();
+	public List add(TouristPlace places)
+	{
+		list.add(places);
+		return list;
+	}
+	public List remove(TouristPlace places)
+	{
+		list.remove(places);
+		return list;
+	}
+	public Object sortByDestination(List places)
+	{
+		Comparator<TouristPlace> cm1=Comparator.comparing(TouristPlace::getDestination);
+		Collections.sort(list, cm1);
+		return cm1;
+	}
+	public Object sortByRank(List places)
+	{
+		list.sort((TouristPlace tp1,TouristPlace tp2)->tp1.getRank().compareTo(tp2.getRank()));
+		return list;
+	}
+	public Object reset(List places)
+	{
+		places.clear();
+		return places;
+	}
+}
